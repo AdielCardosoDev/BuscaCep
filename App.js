13cc6560dc7b5deaf2e5bbafd8a1b80609230586
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2021-10-10 10:46:15 -0300

    Modelo

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./SRC/mapa.png")} style={styles.logo} />
+
+      <View style={styles.imputContainer}>
+        <TextInput
+          placeholder="Exe: 49680000"
+          style={styles.input}
+          keyboardType="number-pad"
+        />
+        <TouchableOpacity style={styles.btn_buscar} activeOpacity={0.7}>
+          <AntDesign name="search1" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.resultadoCard}></View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +28,54 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,    
+    backgroundColor: '#535356',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  logo:{
+    marginTop: '20%',
+    height: 150,
+    width: 150,
+  },
+
+  imputContainer:{
+    marginTop: 50, 
+       
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection: "row" 
+    
+  },  
+
+  input:{    
+    width: 250,
+    height: 35,    
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    paddingStart: 5 
+  },
+
+  btn_buscar:{
+    backgroundColor:'#44BD32',
+    height: 35,
+    width: 50,    
+    borderBottomRightRadius: 5,
+    borderTopEndRadius: 5, 
+    justifyContent: 'center',
+    alignItems: 'center'  
+  },
+
+  resultadoCard:{
+    backgroundColor:"#ffff",
+    height: 200,
+    width: 300,
+    marginTop: 30,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5
+    
+  }
+ 
+
 });
