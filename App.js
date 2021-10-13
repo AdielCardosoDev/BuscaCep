1b8132b90726ad7633d3141fe8bdfe9f944a5d1c
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2021-10-13 10:09:24 -0300

    Final

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons'; 
+import api from './services/api';
 
 export default function App() {
+
+  const [infoCep, setInfoCep] = useState({});
+  const [searcCep, setSearcCep] = useState('');
+
+  const getCep = async () => {
+    const {data} = await api.get(`${searcCep}/json/`);
+    setInfoCep(data);
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("./SRC/mapa.png")} style={styles.logo} />
@@ -11,15 +21,27 @@ export default function App() {
       <View style={styles.imputContainer}>
         <TextInput
           placeholder="Exe: 49680000"
+          placeholderTextColor="#fff"
           style={styles.input}
           keyboardType="number-pad"
+          value={searcCep}
+          onChangeText={(text) => setSearcCep(text)}
         />
-        <TouchableOpacity style={styles.btn_buscar} activeOpacity={0.7}>
+        <TouchableOpacity
+          style={styles.btn_buscar}
+          activeOpacity={0.7}
+          onPress={getCep}
+        >
           <AntDesign name="search1" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
 
-      <View style={styles.resultadoCard}></View>
+      <View style={styles.resultadoCard}>
+        <Text style={styles.resultadoText}>Cidade: {infoCep.localidade} </Text>
+        <Text style={styles.resultadoText}>Bairro: {infoCep.bairro} </Text>
+        <Text style={styles.resultadoText}>Endereço: {infoCep.logradouro}</Text>
+        <Text style={styles.resultadoText}>Estado: {infoCep.uf}</Text>
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -28,53 +50,56 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
-    backgroundColor: '#535356',
-    alignItems: 'center',
-    
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
-  logo:{
-    marginTop: '20%',
+  logo: {
+    marginTop: "20%",
     height: 150,
     width: 150,
   },
 
-  imputContainer:{
-    marginTop: 50, 
-       
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection: "row" 
-    
-  },  
+  imputContainer: {
+    marginTop: 50,
 
-  input:{    
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  input: {
     width: 250,
-    height: 35,    
-    backgroundColor: '#fff',
+    height: 35,
+    backgroundColor: "#535356",
     borderTopLeftRadius: 5,
     borderBottomLeftRadius: 5,
-    paddingStart: 5 
+    paddingStart: 5,
+    color: '#fff'
   },
 
-  btn_buscar:{
-    backgroundColor:'#44BD32',
+  btn_buscar: {
+    backgroundColor: "#44BD32",
     height: 35,
-    width: 50,    
+    width: 50,
     borderBottomRightRadius: 5,
-    borderTopEndRadius: 5, 
-    justifyContent: 'center',
-    alignItems: 'center'  
+    borderTopEndRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
   },
 
-  resultadoCard:{
-    backgroundColor:"#ffff",
+  resultadoCard: {
+    backgroundColor: "#ffff",
     height: 200,
     width: 300,
     marginTop: 30,
     borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5    
-  }
- 
-
+    borderBottomRightRadius: 5,
+    justifyContent: "center",
+  },
+  resultadoText: {
+    fontSize: 18,
+    height: 40,
+    marginStart: 10,
+  },
 });
